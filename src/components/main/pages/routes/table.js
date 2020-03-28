5698b60e678a3a9cb7dd2ae761acc639f515a12c
zetahawke <michel.szinavel@gmail.com>
2020-03-28 00:10:06 -0300

    Feat: Adding data to render inside table

--- src/components/main/pages/routes/table.js
+++ src/components/main/pages/routes/table.js
@@ -1,8 +1,62 @@
 import React from 'react';
+import { Table, Row, Col, Card } from 'antd';
 
-const RoutesTable = results => {
+const { Column } = Table;
+const routesTableProps = {
+  scroll: { x: 490 },
+  size: 'small',
+  showHeader: true
+}
+
+const RoutesTable = ({results}) => {
   return (
     <>
+      <Row gutter={24}>
+        <Col>
+          <Card title={'Rutas encontradas'}>
+            <Row gutter={24}>
+              <Table {...routesTableProps} dataSource={results}>
+                <Column
+                  title='Patente del bus'
+                  dataIndex='patent'
+                  render={
+                    value => {
+                      return value;
+                    }
+                  }
+                />
+                <Column
+                  title='Hora de salida'
+                  dataIndex='out'
+                  render={
+                    value => {
+                      return value
+                    }
+                  }
+                />
+                <Column
+                  title='Hora de llegada'
+                  dataIndex='arrive'
+                  render={
+                    value => {
+                      return value
+                    }
+                  }
+                />
+                <Column
+                  title='Estado del viaje'
+                  dataIndex='status'
+                  render={
+                    value => {
+                      return value
+                    }
+                  }
+                />
+              </Table>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
     </>
   );
 }
