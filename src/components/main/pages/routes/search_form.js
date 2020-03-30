a1a06d1522d807b6c9d15795cfd157c2b38e5889
zetahawke <michel.szinavel@gmail.com>
2020-03-29 23:04:33 -0300

    - split search form component

---
+++ src/components/main/pages/routes/search_form.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Select, DatePicker, Input, Form, Button } from 'antd';
+import moment from 'moment';
+
+const RoutesSearchForm = ({onFinish}) => {
+  return (
+    <Form layout='inline' onFinish={onFinish}>
+      <Form.Item label='Destino' name='destiny'>
+        <Select defaultValue="stg">
+          <Select.Option value="stg">Santiago / Peor es Nada</Select.Option>
+          <Select.Option value="psn">Peor es Nada / Santiago</Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item label='Fecha' name='date'>
+        <DatePicker defaultValue={moment()} />
+      </Form.Item>
+      <Form.Item label='Patente (opcional)' name='patent'>
+        <Input placeholder='ABCD12' />
+      </Form.Item>
+      <Form.Item>
+        <Button type='primary' htmlType='submit'>Buscar</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default RoutesSearchForm
